3c85980255756c5c3051f847e93b020068069c08
XIMIRXX <imirbadirli466@gmail.com>
2026-03-27 21:33:51 +0300

    Refactor encryption and decryption into caesar function

--- main.py
+++ main.py
@@ -5,28 +5,19 @@ print(art.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def encrypt(original_text, shift_amount):
+def caesar(original_text, shift_amount, encode_or_decode):
     cipher_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
     for letter in original_text:
         if letter not in alphabet:
             cipher_text += letter
             continue
+
         shifted_position = alphabet.index(letter) + shift_amount
         shifted_position %= len(alphabet)
         cipher_text += alphabet[shifted_position]
-    print(f"Here is the encoded result: {cipher_text}")
-
-def decrypt(original_text, shift_amount):
-    cipher_text = ""
-    for letter in original_text:
-        if letter not in alphabet:
-            cipher_text += letter
-            continue
-        anti_shifted_position = alphabet.index(letter) - shift_amount
-        anti_shifted_position %= len(alphabet)
-        cipher_text += alphabet[anti_shifted_position]
-    print(f"Here is the decoded result: {cipher_text}")
-
+    print(f"Here is the result: {cipher_text}")
 
 while True:
 
@@ -37,10 +28,7 @@ while True:
     text = input("Type your message: ").lower()
     shift = int(input("Type the shift number: "))
 
-    if direction == "encode":
-        encrypt(original_text=text, shift_amount=shift)
-    elif direction == "decode":
-        decrypt(original_text=text, shift_amount=shift)
+    caesar(text, shift, direction)
     final_answer = input("Do you want to continue? (yes/no): ")
     if final_answer == "no":
         break
